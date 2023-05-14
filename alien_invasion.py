7aa8409d96102ab06135fa72eb890d907ac6344e
imKevinFlynn <mrproper0990@gmail.com>
2023-05-15 02:44:53 +0300

    Added functionality to strike with bullets, refactored the code.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -11,17 +12,23 @@ class AlienInvasion:
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,
-                                              self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")  # название окна
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            # Отслеживание событий клавиатуры и мыши.
+            # Отслеживание событий клавиатуры, мыши и обновление расположения снарядов,
+            # корабля.
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._update_bullets()
+            #print(len(self.bullets)) #проверка для удаления снарядов
             self._update_screen()
 
     def _check_events(self):
@@ -30,29 +37,50 @@ class AlienInvasion:
                 sys.exit()
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Переместить корабль вправо.
-                    self.ship.moving_right = True
-                    # Переместить корабль влево.
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    # Переместить корабль вправо.
-                    self.ship.moving_right = False
-                    # Переместить корабль влево.
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатие клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Реагирует на отпускание клавиш."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        # удаление снарядов, вышедших за край экрана для освобождения памяти.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         # При каждом проходе цикла обновляется изображение на экране.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Отображение последнего прорисованного изображения.
         pygame.display.flip()
-
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -8,4 +8,10 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 800
         self.bg_color = (134, 143, 175)  # цвет фона
-
+        self.ship_speed = 1.5  # скорость корабля
+        # параметры снарядов корабля
+        self.bullet_speed = 0.95
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullets_allowed = 3
+        self.bullet_color = (60, 60, 60)
