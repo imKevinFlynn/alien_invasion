4846198210f1fe435bee96df5e33489dc0446be8
imKevinFlynn <mrproper0990@gmail.com>
2023-05-17 09:51:40 +0300

    Create the fleet of aliens at the top of the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,9 +78,29 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Создание флота вторжения."""
-        # Создание пришельца.
+        # Создание пришельца и вычисление количества пришельцев в ряду
+        # Интервал между соседними пришельцами равен ширине пришельца.
         alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Создание первого ряда пришельцев.
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        """Создание пришельца и размещение его в ряду."""
+        # Создание пришельца и размещение его в ряду.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
         self.aliens.add(alien)
+
+
+
+
 
     def _update_screen(self):
         # При каждом проходе цикла обновляется изображение на экране.
